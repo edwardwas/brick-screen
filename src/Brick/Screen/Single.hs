d31f73f0575621a0e33e12b3c74cef2ee978b9ac
Ed Wastell <ed@wastell.co.uk>
2019-04-14 19:40:00 +0100

    Some profunctor instances

--- src/Brick/Screen/Single.hs
+++ src/Brick/Screen/Single.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RankNTypes    #-}
+{-# LANGUAGE TupleSections #-}
 
 module Brick.Screen.Single where
 
@@ -7,19 +8,38 @@ import           Brick
 import           Control.Lens
 import           Control.Monad.State
 import           Control.Monad.Writer
+import           Data.Profunctor
+import           Data.Profunctor.Choice
+import           Data.Profunctor.Strong
 
-data SingleScreen n e s f a = SingleScreen
+data SingleScreen n s f e a = SingleScreen
   { drawSingleScreen   :: s -> Widget n
   , updateSingleScreen :: e -> s -> f (a, s)
   } deriving (Functor)
 
+instance Functor f => Profunctor (SingleScreen n s f) where
+    lmap f (SingleScreen draw update) =
+        SingleScreen draw (\e s -> update (f e) s)
+    rmap = fmap
+
+
+instance Functor f => Strong (SingleScreen n s f) where
+    first' (SingleScreen draw update) =
+        SingleScreen draw $ \(e, c) s -> over _1 (, c) <$> update e s
+
+instance Applicative f => Choice (SingleScreen n s f) where
+    left' (SingleScreen draw update) =
+        let helper (Left e) s  = over _1 Left <$> update e s
+            helper (Right c) s = pure (Right c,s)
+         in SingleScreen draw helper
+
 hoistSingleScreen ::
-     (forall x. f x -> g x) -> SingleScreen n e s f a -> SingleScreen n e s g a
+     (forall x. f x -> g x) -> SingleScreen n s f e a -> SingleScreen n s g e a
 hoistSingleScreen func (SingleScreen draw update) =
   SingleScreen draw (\e s -> func $ update e s)
 
 zoomSingleScreen ::
-     Functor f => Lens' s t -> SingleScreen n e t f a -> SingleScreen n e s f a
+     Functor f => Lens' s t -> SingleScreen n t f e a -> SingleScreen n s f e a
 zoomSingleScreen l (SingleScreen draw update) =
   SingleScreen
     { drawSingleScreen = \t -> draw (t ^. l)
@@ -30,8 +50,8 @@ zoomMultiSingleScreen ::
      (Monad f, Monoid a)
   => (Widget n -> Widget n -> Widget n)
   -> Traversal' s t
-  -> SingleScreen n e t f a
-  -> SingleScreen n e s f a
+  -> SingleScreen n t f e a
+  -> SingleScreen n s f e a
 zoomMultiSingleScreen combineFunc l (SingleScreen draw update) =
   SingleScreen
     { drawSingleScreen = foldlOf (l . to draw) combineFunc emptyWidget
--- src/Brick/Screen/Stack.hs
+++ src/Brick/Screen/Stack.hs
@@ -13,30 +13,37 @@ import           Control.Lens
 import           Control.Monad.Free
 import           Control.Monad.State
 import           Data.Monoid             (Ap (..))
+import           Data.Profunctor
+import           Data.Profunctor.Choice
+import           Data.Profunctor.Strong
 import qualified Graphics.Vty            as V
 
-newtype ScreenStack n e s f a =
-  ScreenStack (Free (SingleScreen n e s f) a)
+newtype ScreenStack n s f e a =
+  ScreenStack (Free (SingleScreen n s f e) a)
     deriving (Functor, Applicative, Monad)
 
+instance Functor f => Profunctor (ScreenStack n s f) where
+  lmap func (ScreenStack f) = ScreenStack $ hoistFree (lmap func) f
+  rmap = fmap
+
 screen ::
-     Functor f => (s -> Widget n) -> (e -> s -> f (a, s)) -> ScreenStack n e s f a
+     Functor f => (s -> Widget n) -> (e -> s -> f (a, s)) -> ScreenStack n s f e a
 screen draw update = ScreenStack $ liftF $ SingleScreen draw update
 
 screenState ::
-     Functor f => (s -> Widget n) -> (e -> StateT s f a) -> ScreenStack n e s f a
+     Functor f => (s -> Widget n) -> (e -> StateT s f a) -> ScreenStack n s f e a
 screenState draw update = screen draw (\e s -> runStateT (update e) s)
 
 hoistScreenStack ::
      Functor g
   => (forall x. f x -> g x)
-  -> ScreenStack n e s f a
-  -> ScreenStack n e s g a
+  -> ScreenStack n s f e a
+  -> ScreenStack n s g e a
 hoistScreenStack func (ScreenStack inner) =
   ScreenStack $ hoistFree (hoistSingleScreen func) inner
 
 zoomScreenStack ::
-     Functor f => Lens' s t -> ScreenStack n e t f a -> ScreenStack n e s f a
+     Functor f => Lens' s t -> ScreenStack n t f e a -> ScreenStack n s f e a
 zoomScreenStack l (ScreenStack inner) =
   ScreenStack $ hoistFree (zoomSingleScreen l) inner
 
@@ -44,8 +51,8 @@ zoomMultiScreenStack ::
      (Monoid a, Monad f)
   => (Widget n -> Widget n -> Widget n)
   -> Traversal' s t
-  -> ScreenStack n e t f a
-  -> ScreenStack n e s f a
+  -> ScreenStack n t f e a
+  -> ScreenStack n s f e a
 zoomMultiScreenStack combineFunc l (ScreenStack inner) =
   let helper (Free screen) =
         Free
@@ -54,7 +61,7 @@ zoomMultiScreenStack combineFunc l (ScreenStack inner) =
       helper (Pure a) = Pure a
    in ScreenStack $ helper inner
 
-screenStackApp :: App (s, ScreenStack n (BrickEvent n e) s TrackedIO a) e n
+screenStackApp :: App (s, ScreenStack n s TrackedIO (BrickEvent n e) a) e n
 screenStackApp =
   let eventHelper (s, ScreenStack (Free (SingleScreen _ update))) e =
         let helper (Free f, s') = continue (s', ScreenStack $ Free f)
@@ -74,5 +81,5 @@ screenStackApp =
         }
 
 screenStackMain ::
-     Ord n => ScreenStack n (BrickEvent n e) s TrackedIO () -> s -> IO s
+     Ord n => ScreenStack n s TrackedIO (BrickEvent n e) () -> s -> IO s
 screenStackMain stack s = fst <$> defaultMain screenStackApp (s, stack)
